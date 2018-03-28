420d5347e3f18fa2aea02fd98fa3ba720c782776
Saulo Nunes <saulo@paralink.com.br>
2018-03-28 07:23:44 -0300

    Add Skin

--- Delphi/ValidadorCartaoCredito/App/CartaoCredito.pas
+++ Delphi/ValidadorCartaoCredito/App/CartaoCredito.pas
@@ -43,89 +43,23 @@ uses System.SysUtils, Vcl.Forms, Winapi.Windows, ACBrValidador;
 procedure TCartaoCredito.CheckFlag(const oCartaoCredito: TCartaoCredito);
 Var
   FlagNumber1, FlagNumber2 : Integer;
-  newStr : string;
 begin
-  newStr := StringReplace(Self.Numero, ' ', '', [rfReplaceAll]);
-  FlagNumber1 := StrToInt(Copy(newStr, 1, 1));
-  //
-  If FlagNumber1 in [3, 5] then
-    FlagNumber2 := StrToInt(Copy(newStr, 1, 2))
-  Else If FlagNumber1 = 6 then
-    FlagNumber2 := StrToInt(Copy(newStr, 1, 4))
+  FlagNumber1 := StrToInt(Copy(Self.Numero, 1, 1));
+  ///
+  If FlagNumber1 in [3, 5] then 
+    FlagNumber2 := StrToInt(Copy(Self.Numero, 1, 2))
+  Else If FlagNumber1 = 6 then 
+    FlagNumber2 := StrToInt(Copy(Self.Numero, 1, 4))
   Else 
-    FlagNumber2 := FlagNumber1;
+    FlagNumber2 := FlagNumber1; 
   //
   case FlagNumber2 of
-    4      :
-      Begin
-        if (length(newStr) = 13) Or (length(newStr) = 16) then
-        Begin
-          Self.Ok := True;
-          Self.Bandeira := 'VISA';
-        End
-        Else
-        Begin
-          Self.Bandeira := 'ALL';
-          Self.Ok := False;
-        End;
-      End;
-    34     :
-      Begin
-        if length(newStr) = 15 then
-        Begin
-          Self.Ok := True;
-          Self.Bandeira := 'AMEX';
-        End
-        Else
-        Begin
-          Self.Bandeira := 'ALL';
-          Self.Ok := False;
-        End;
-      End;
-    37     :
-      Begin
-        if length(newStr) = 15 then
-        Begin
-          Self.Ok := True;
-          Self.Bandeira := 'AMEX';
-        End
-        Else
-        Begin
-          Self.Bandeira := 'ALL';
-          Self.Ok := False;
-        End;
-      End;
-    51..55 :
-      Begin
-        if length(newStr) = 16 then
-        Begin
-          Self.Ok := True;
-          Self.Bandeira := 'MASTERCARD';
-        End
-        Else
-        Begin
-          Self.Bandeira := 'ALL';
-          Self.Ok := False;
-        End;
-      End;
-    6011   :
-      Begin
-        if length(newStr) = 16 then
-        Begin
-          Self.Ok := True;
-          Self.Bandeira := 'DISCOVER';
-        End
-        Else
-        Begin
-          Self.Bandeira := 'ALL';
-          Self.Ok := False;
-        End;
-      End
-    Else
-      Begin
-        Self.Bandeira := 'All';
-        Self.Ok       := False;
-      End;
+    4      : Self.Bandeira := 'Visa';
+    34     : Self.Bandeira := 'AMEX';
+    37     : Self.Bandeira := 'AMEX';
+    51..55 : Self.Bandeira := 'MasterCard';
+    6011   : Self.Bandeira := 'Discover';
+    Else     Self.Bandeira := 'All';
   end;
 end;
 
@@ -198,12 +132,11 @@ procedure TCartaoCredito.ValidateCreditCard(const oCartaoCredito
 begin
   Self.CheckFlag(Self);
   //
-  if Self.Ok then
-    case Self.ValidationType of
-      0: CheckLUHNAcbr(Self);
-      1: Self.Ok := CheckLUHN(oCartaoCredito.Numero);
-      2: CheckLUHNDLL(Self);
-    end;
+  case Self.ValidationType of
+    0: CheckLUHNAcbr(Self);
+    1: Self.Ok := CheckLUHN(oCartaoCredito.Numero);
+    2: CheckLUHNDLL(Self);
+  end;
 end;
 
 end.
