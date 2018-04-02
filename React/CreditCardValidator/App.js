36a7e7ad53713be4b35c1c5b812de32b2f615d5b
Saulo Nunes <saulo@paralink.com.br>
2018-04-02 05:11:10 -0300

    Primeiro AJuste

--- React/CreditCardValidator/App.js
+++ React/CreditCardValidator/App.js
@@ -1,75 +1,58 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Switch } from "react-native";
-import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input"; // 0.4.1
+import { StyleSheet, View, Text, KeyboardAvoidingView  } from "react-native";
+import { CreditCardInput } from "react-native-credit-card-input"; 
 
-const s = StyleSheet.create({
-  switch: {
-    alignSelf: "center",
-    marginTop: 20,
-    marginBottom: 20,
-  },
+export default class App extends Component {
+
+  _onChange = (formData) => console.log(JSON.stringify(formData, null, " "));
+
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={style.Title} >Validador de Cartão </Text>
+        <KeyboardAvoidingView behavior="padding">
+            <CreditCardInput
+
+              requiresName = {false}
+              requiresCVC = {false}
+              requiresPostalCode = {false}
+
+              labels = {{ number: "NÚMERO", expiry: "VALIDADE", cvc: "CVC/CCV" }}
+              placeholders = {{expiry: "MM/AA"}}
+
+              cardScale={0.7}
+              labelStyle={style.label}
+              inputStyle={style.input}
+              validColor={"black"}
+              invalidColor={"red"}
+              placeholderColor={"darkgray"} 
+              onChange={this._onChange}
+              />
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',    
     backgroundColor: "#F5F5F5",
-    marginTop: 60,
   },
+  Title: {
+    alignSelf: "center",
+    fontSize: 16,
+    marginVertical: 10,
+    color: "black"
+  },  
   label: {
     color: "black",
     fontSize: 12,
   },
   input: {
-    fontSize: 16,
+    fontSize: 12,
     color: "black",
-  },
-});
-
-
-export default class Example extends Component {
-  state = { useLiteCreditCardInput: true };
-
-  _onChange = (formData) => console.log(JSON.stringify(formData, null, " "));
-  _onFocus = (field) => console.log("focusing", field);
-  _setUseLiteCreditCardInput = (useLiteCreditCardInput) => this.setState({ useLiteCreditCardInput });
-
-  render() {
-    return (
-      <View style={s.container}>
-        <Switch
-          style={s.switch}
-          onValueChange={this._setUseLiteCreditCardInput}
-          value={this.state.useLiteCreditCardInput} />
-
-        { this.state.useLiteCreditCardInput ?
-          (
-            <LiteCreditCardInput
-              autoFocus
-              
-              inputStyle={s.input}
-              validColor={"black"}
-              invalidColor={"red"}
-              placeholderColor={"darkgray"}
-
-              onFocus={this._onFocus}
-              onChange={this._onChange} />
-          ) : (
-            <CreditCardInput
-              autoFocus
-
-              requiresName
-              requiresCVC
-              requiresPostalCode
-
-              cardScale={1.0}
-              labelStyle={s.label}
-              inputStyle={s.input}
-              validColor={"black"}
-              invalidColor={"red"}
-              placeholderColor={"darkgray"}
-
-              onFocus={this._onFocus}
-              onChange={this._onChange} />
-          )
-        }
-      </View>
-    );
   }
-}
+});
